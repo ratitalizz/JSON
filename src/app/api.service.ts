f6a5016cc2108805af9a2c85a76695bb027ed9db
ratitalizz <barocae@gmail.com>
2021-10-06 12:24:11 -0300

    Confirmo 6-10-2021

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -14,11 +14,35 @@ export class APIService {
       'Access-Control-Allow-Origin':'**'
     })
   }
+direccion="https://jsonplaceholder.typicode.com/posts"
 
   constructor(private http: HttpClient) {}
-  
+
+  //Permite recuperar todos los registros
+
   getPosts(): Observable<any>{
-    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+    return this.http.get(this.direccion)
     .pipe(retry(3) );
+  }
+  
+  getPost(id):Observable<any>{
+    return this.http.get(this.direccion+'/'+id).pipe(retry(3));
+  }
+
+//Metodo que permite recuperar el valor del dolar
+  getDolar():Observable<any>{
+    return this.http.get('https://mindicador.cl/api/dolar').pipe(retry(3));
+  }
+//Método que permite almacenar o inyectar datos
+  crearPost(post):Observable<any>{
+    return this.http.post(this.direccion,post, this.httpOptions).pipe(retry(3));
+  }
+//Método para actualizar registro
+  updatePost(id,post):Observable<any>{
+    return this.http.put(this.direccion+'/'+id, post, this.httpOptions).pipe(retry(2));
+  }
+//Método para eliminar
+deletePost(id):Observable<any>{
+  return this.http.delete(this.direccion+'/'+id, this.httpOptions).pipe(retry(2));
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+// Importar librerías
+import { APIService } from '../api.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +8,59 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private apiServ: APIService) { }
 
+  recuperarTodo() {
+    this.apiServ.getPosts().subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
+  recuperarID() {
+    this.apiServ.getPost(20).subscribe(
+      (data) => { console.log(data); },
+      (error) => { console.log(error); }
+
+    );
+  }
+  recuperarDolar() {
+    this.apiServ.getDolar().subscribe(
+      (data) => { console.log(data.serie[0].valor); },
+      (error) => { console.log(error); }
+    );
+  }
+  crearPost() {
+    var post = {
+      userId: 1,
+      id: 101,
+      title: 'Hola mundo',
+      body: 'Hola lindo mundo'
+    };
+    this.apiServ.crearPost(post).subscribe(
+      (success) => { console.log(success); },
+      (error) => { console.log(error); }
+    )
+  }
+  updatePost() {
+    var post = {
+      userId: 1,
+      id: 20,
+      title: 'Hola mundo',
+      body: 'Hola lindo mundo'
+    };
+    this.apiServ.updatePost(20, post).subscribe(
+      (success) => { console.log(success); },
+      (error) => { console.log(error); } 
+    )
+  }
+  deletePost(){
+    this.apiServ.deletePost(20).subscribe(
+      (success) => { console.log(success); },
+      (error) => { console.log(error); } 
+    )
+  }
 }
